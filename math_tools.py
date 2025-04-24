c582adffa5390e31765ff74a80144822d1a21a47
Sydney Rowe <sydneyrowe@wifi-10-42-245-131.wifi.berkeley.edu>
2025-04-23 20:33:52 -0700

    math_tools.py

---
+++ math_tools.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    
+   # Returns the sum of a and b.
+  
+    return a + b
+
+def subtract(a, b):
+
+    #Returns the difference between a and b (a - b).
+    
+    return a - b
+
+def multiply(a, b):
+    
+    #Returns the product of a and b.
+    
+    return a * b
+
+def divide(a, b):
+
+    #Returns the quotient of a divided by b.
+    If b is zero, returns 'Undefined' to avoid division by zero.
+    
+    if b == 0:
+        return "Undefined"
+    return a / b
